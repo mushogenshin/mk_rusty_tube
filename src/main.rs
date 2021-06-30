d3b4635b725a92103ef8b26a4541b42c1a14c946
hoan <mushogenshin@gmail.com>
2021-06-30 20:48:40 +0700

    added min_size for app; rebuilt for Windows

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use rusty_tube::RustyTubeDL;
 fn main() -> iced::Result {
     let window_settings = window::Settings {
         size: (600, 170),
+        min_size: Some((570, 155)),
         ..window::Settings::default()
     };
     let app_settings = settings::Settings {
